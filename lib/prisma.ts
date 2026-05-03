59d70e071254a2e6fbb42f993e9a5c151da54024
shubhamasthana56 <44361564+shubhamasthana56@users.noreply.github.com>
2026-05-03 17:41:17 +0530

    fixed prisma db issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,12 @@
 // Dynamic Prisma client to avoid initialization during build
 import { PrismaClient } from '@prisma/client'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import Database from 'better-sqlite3'
 
 let prismaInstance: any = null
 
 export const getPrisma = async () => {
   if (!prismaInstance) {
-    const adapter = new PrismaBetterSqlite3(new Database('./dev.db'))
+    const adapter = new PrismaBetterSqlite3({ url: './dev.db' })
 
     prismaInstance = new PrismaClient({
       adapter,
